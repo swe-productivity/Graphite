1bf9abd72f502971505d1f1bd1a38ded49a73217
Timon <me@timon.zip>
2025-11-12 10:33:16 -0800

    icon

--- desktop/src/window/win.rs
+++ desktop/src/window/win.rs
@@ -13,6 +13,7 @@ impl super::NativeWindow for NativeWindowImpl {
 			let icon = winit::icon::Icon(std::sync::Arc::new(win_icon));
 			attributes.with_window_icon(Some(icon))
 		} else {
+			tracing::warn("Failed to load icon from resource");
 			attributes
 		}
 	}

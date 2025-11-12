e764e979960389c34333a8dfa3bde1915f82930d
Timon Schelling <me@timon.zip>
2025-11-12 18:22:40 +0100

    fix

--- desktop/platform/win/src/main.rs
+++ desktop/platform/win/src/main.rs
@@ -1,7 +1,7 @@
-windows::Win32::System::Console::{AttachConsole, ATTACH_PARENT_PROCESS};
-
 #![windows_subsystem = "windows"]
+
 fn main() {
+	use windows::Win32::System::Console::{ATTACH_PARENT_PROCESS, AttachConsole};
 	let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
 
 	graphite_desktop::start();

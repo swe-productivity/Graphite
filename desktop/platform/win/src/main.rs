938573a757fa14279349bc132196f5050b5bf8b3
Timon <me@timon.zip>
2025-11-12 09:20:29 -0800

    attach to parent process console

--- desktop/platform/win/src/main.rs
+++ desktop/platform/win/src/main.rs
@@ -1,4 +1,8 @@
+windows::Win32::System::Console::{AttachConsole, ATTACH_PARENT_PROCESS};
+
 #![windows_subsystem = "windows"]
 fn main() {
+	let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
+
 	graphite_desktop::start();
 }
